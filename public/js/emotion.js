cf37e6b79cc064319c4d495962aaad53f12dc153
Ross Ingram <rosingram@gmail.com>
2017-03-17 15:58:36 -0700

    cleaning up

---
+++ public/js/emotion.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+function emotion() {
+};
+
+emotion = new emotion();
+number = 0;
+
+// textArray
+emotion[number++] = "HAPPY"
+emotion[number++] = "SAD"
+emotion[number++] = "EXCITED"
+emotion[number++] = "BORED"
+emotion[number++] = "WORRIED"
+emotion[number++] = "OPTIMISTIC"
+emotion[number++] = "HOPEFUL"
+emotion[number++] = "JEALOUS"
+emotion[number++] = "STRESSED"
+emotion[number++] = "GUILTY"
+emotion[number++] = "IN.LOVE"
+emotion[number++] = "SHAMEFUL"
+emotion[number++] = "COURAGEOUS"
+emotion[number++] = "PRIDEFUL"
+emotion[number++] = "ENVIOUS"
+emotion[number++] = "PLAYFUL"
+
+
+
+
+// keep adding items here...
+
+increment = Math.floor(Math.random() * number);
---
+++ public/js/relationship.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+
+function relationship() {
+};
+
+relationship = new relationship();
+number = 0;
+
+// textArray
+relationship[number++] = "ARE.FRIENDS"
+relationship[number++] = "ARE.IN.LOVE"
+relationship[number++] = "KNOW.EACH.OTHER"
+relationship[number++] = "LIVE.TOGETHER"
+relationship[number++] = "ACCOMPANY"
+relationship[number++] = "GET.ALONG.WELL"
+relationship[number++] = "LEAVES.BEHIND"
+relationship[number++] = "ENJOYS.DOING"
+relationship[number++] = "IS.WELL"
+relationship[number++] = "IS.UNWELL"
+relationship[number++] = "CREATES"
+relationship[number++] = "IS.TRANSFORMING.INTO"
+relationship[number++] = "TROUBLES"
+relationship[number++] = "HAS.THOUGHTS.ABOUT"
+relationship[number++] = "HAS.DREAMS.ABOUT"
+relationship[number++] = "VALUES"
+relationship[number++] = "WANTS"
+relationship[number++] = "TAKES.PLEASURE.IN"
+relationship[number++] = "INSPIRES"
+relationship[number++] = "WORKS.WITH"
+relationship[number++] = "PLAYS.WITH"
+relationship[number++] = "TAKE.WITH"
+relationship[number++] = "MOTIVATES"
+relationship[number++] = "AGREES.WITH"
+relationship[number++] = "GIVES"
+
+
+// keep adding items here...
+
+increment = Math.floor(Math.random() * number);
