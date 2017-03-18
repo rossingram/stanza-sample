747e033270ff1b92868383aa72c2007932f3cceb
Ross Ingram <rosingram@gmail.com>
2017-03-17 18:38:58 -0700

    adding actors

--- public/js/actors_one.js
+++ public/js/actors_one.js
@@ -4,18 +4,45 @@ function actors_one() {
 };
 
 actors_one = new actors_one();
-number = 10;
+number = 0;
 
 // textArray
 actors_one[number++] = "THING"
+actors_one[number++] = "CAT"
+actors_one[number++] = "DOG"
 actors_one[number++] = "I"
 actors_one[number++] = "YOU"
 actors_one[number++] = "OTHER"
+actors_one[number++] = "DREAM"
 actors_one[number++] = "LIFE"
 actors_one[number++] = "MOTIVATION"
+actors_one[number++] = "LOVE"
+actors_one[number++] = "FRIENDSHIP"
+actors_one[number++] = "MUSIC"
+actors_one[number++] = "SHOE"
+actors_one[number++] = "BOOK"
+actors_one[number++] = "FOOD"
+actors_one[number++] = "HONESTY"
+actors_one[number++] = "HOPE"
+actors_one[number++] = "LAUGHTER"
+actors_one[number++] = "MIND"
+actors_one[number++] = "SPIRIT"
 actors_one[number++] = "BODY"
+actors_one[number++] = "HOME"
+actors_one[number++] = "TRAVEL"
+actors_one[number++] = "WORK"
+actors_one[number++] = "PLAY"
+actors_one[number++] = "EMOTION/FEELING"
+actors_one[number++] = "SOMEONE"
+actors_one[number++] = "SOMETHING"
+actors_one[number++] = "THOUGHT"
+actors_one[number++] = "EARTH"
+actors_one[number++] = "COMFORT"
 actors_one[number++] = "FAMILY"
+actors_one[number++] = "GOD"
 actors_one[number++] = "SCIENCE"
+actors_one[number++] = "PERSON"
 actors_one[number++] = "ROBOT"
+actors_one[number++] = "COMPUTER"
 
-increment = Math.floor(Math.random() * number);
+increment = Math.floor(Math.random() * number);ß\
